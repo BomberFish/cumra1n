54c909c16e77be3558ffc0def6dee8f13a07647f
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-11-25 00:01:32 -0500

    Add iOS 17 support

--- ballpa1n/jelbraik/ContentView.swift
+++ ballpa1n/jelbraik/ContentView.swift
@@ -51,7 +51,7 @@ struct ContentView: View {
             }
             HStack {
             #if os(iOS) || os(iPadOS) || os(tvOS) || os(watchOS)
-                Text("\(UIDevice.current.systemName) 1.0 - 16.3 Jailbreak")
+                Text("\(UIDevice.current.systemName) 1.0 - 17.0 Jailbreak")
                     .font(.system(.body, design: .monospaced))
                     .foregroundColor(fg)
                 #elseif os(macOS)
